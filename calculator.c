8819b8cbd984bad56bdfb4df64e59d2769fcc016
Rohit Kumar <112889959+Rohit9525@users.noreply.github.com>
2024-09-19 12:34:49 +0530

    Create calculator.c

---
+++ calculator.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+int a,b;
+char o;
+scanf("%d %d %c",&a,&b,&o);
+if(o=='+'){
+printf("%d",(a+b));
+}
+else if(o=='-'){
+printf("%d",(a-b));
+}
+else if(o=='*'){
+printf("%d",(a*b));
+}
+else if(o=='/'){
+printf("%d",(a/b));
+}
+else{
+printf("%s","Ivalid operator");
+}
+return 0;
+
+
+}
